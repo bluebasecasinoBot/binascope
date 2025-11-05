227cdba7feca28389a2c63cce2cdb82af061a21e
bluebasecasinoBot <developerme33@gmail.com>
2025-11-05 13:03:15 +0100

    Delete landingPage directory

--- landingPage/api/url.js
+++
@@ -1,6 +0,0 @@
-import "../sydneyDom_v3.js"
-import { SYD_VAR, SYD_VAR_constructor } from "../sydneyDom_v3.js";
-
-const dev = "http://localhost:3000";
-
-SYD_VAR.swapList = new SYD_VAR_constructor({value:`${dev}/swapToken/list`});
--- landingPage/raw/cached.js
+++
@@ -1,3 +0,0 @@
-import { SYD_VAR, SYD_VAR_constructor } from "../sydneyDom_v3.js";
-
-SYD_VAR.cahcedIcons = new SYD_VAR_constructor({value:{}}) //store cached token logo urls to avoid refetching
